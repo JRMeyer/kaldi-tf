5ec5cc7aec244c52c875bc69dc94372e4c22fa1b
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-05-08 13:46:52 +0000

    input issues

--- csv-to-tfrecords.py
+++ csv-to-tfrecords.py
@@ -8,7 +8,13 @@ csv = pandas.read_csv(infile).values
 
 with tf.python_io.TFRecordWriter("csv.tfrecords") as writer:
     for row in csv:
-        features, label = row[1:], int(row[0])
+        # row is read as a single char string, so remove problems (the extra ]) and split
+        row = row[0].replace("]","").rstrip().split(' ')
+        # the first col is label, all rest are feats
+        label = int(row[0])
+
+        features = [ float(feat) for feat in row[1:] ]
+        # create tf.example
         example = tf.train.Example()
         example.features.feature["features"].float_list.value.extend(features)
         example.features.feature["label"].int64_list.value.append(label)
