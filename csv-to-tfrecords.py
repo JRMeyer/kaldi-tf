919f92ee246e821f4bd87573e64464b6bbca2fe1
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-05-09 16:49:03 +0000

    took out kmeans and just managed to get in data

--- csv-to-tfrecords.py
+++ csv-to-tfrecords.py
@@ -1,10 +1,11 @@
 import sys
 import pandas
 import tensorflow as tf
-
+import numpy as np
 
 infile=sys.argv[1]
 csv = pandas.read_csv(infile).values
+
 
 with tf.python_io.TFRecordWriter("csv.tfrecords") as writer:
     for row in csv:
@@ -13,9 +14,9 @@ with tf.python_io.TFRecordWriter("csv.tfrecords") as writer:
         # the first col is label, all rest are feats
         label = int(row[0])
 
-        features = [ float(feat) for feat in row[1:] ]
-        # create tf.example
+        mfccs = np.array([ float(feat) for feat in row[1:] ]).tostring()
+        
         example = tf.train.Example()
-        example.features.feature["features"].float_list.value.extend(features)
+        example.features.feature["mfccs"].bytes_list.value.append(mfccs)
         example.features.feature["label"].int64_list.value.append(label)
         writer.write(example.SerializeToString())
---
+++ input.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+import sys
+
+
+
+
+def parser(record):
+  features={
+    'mfccs': tf.FixedLenFeature([], tf.string),
+    'label': tf.FixedLenFeature([], tf.int64),
+  }
+
+  # # A `tf.data.Dataset` whose elements are dictionaries mapping feature names
+  # # (in this case 'seq') to tensors, based on `features`.
+  parsed = tf.parse_single_example(record, features)
+  
+  mfccs= tf.decode_raw(parsed['mfccs'], tf.float64)
+  label= tf.cast(parsed['label'], tf.int32)
+  
+  return label, mfccs
+
+
+def input_fn(filenames):
+  
+  dataset = tf.data.TFRecordDataset(filenames)
+  dataset = dataset.map(parser)
+  iterator = dataset.make_one_shot_iterator()
+  label, mfccs = iterator.get_next()
+
+  return label, mfccs
+  
+
+if __name__ == "__main__":
+
+  # for example in tf.python_io.tf_record_iterator("/home/ubuntu/csv.tfrecords"):
+        # result = tf.train.Example.FromString(example)
+        # print(result)
+        # exit()
+
+  with tf.Session() as sess:
+        ex = sess.run(input_fn('/home/ubuntu/csv.tfrecords'))
+        print(ex)
+
