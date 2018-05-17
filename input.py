1db7b9f0a342d4589dd9598e20ba556f99d24c81
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-05-17 18:16:31 +0000

    lots of changes following a blog from TF folks

--- input.py
+++ input.py
@@ -1,50 +1,69 @@
-import numpy as np
 import tensorflow as tf
-import sys
 
 
 
+
+  
 
 def parser(record):
   features={
     'mfccs': tf.FixedLenFeature([], tf.string),
     'label': tf.FixedLenFeature([], tf.int64),
   }
-
-  # # A `tf.data.Dataset` whose elements are dictionaries mapping feature names
-  # # (in this case 'seq') to tensors, based on `features`.
-  parsed = tf.parse_single_example(record, features)
-  
-  mfccs= tf.decode_raw(parsed['mfccs'], tf.float64)
+  parsed = tf.parse_single_example(record, features)  
+  mfccs= tf.convert_to_tensor(tf.decode_raw(parsed['mfccs'], tf.float64))
   label= tf.cast(parsed['label'], tf.int32)
-  
-  return label, mfccs
+
+  return {'mfccs': mfccs}, label
 
 
-def input_fn(filenames):
-  
-  dataset = tf.data.TFRecordDataset(filenames)
+def my_input_fn(filepath='/home/ubuntu/csv.tfrecords'):
+  dataset = tf.data.TFRecordDataset(filepath)
   dataset = dataset.map(parser)
+  dataset = dataset.shuffle(buffer_size=256)
+  dataset = dataset.batch(32)
+  
   iterator = dataset.make_one_shot_iterator()
-  label, mfccs = iterator.get_next()
 
-  return label, mfccs
-  
+  batch_mfccs, batch_labels = iterator.get_next()
 
-if __name__ == "__main__":
-
-  # for example in tf.python_io.tf_record_iterator("/home/ubuntu/csv.tfrecords"):
-      # result = tf.train.Example.FromString(example)
-      # print(result)
-      # exit()
-  
-  with tf.Session() as sess:
-        label, mfccs = sess.run(input_fn('/home/ubuntu/csv.tfrecords'))
-        print(label)
+  return batch_mfccs, batch_labels
 
 
+with tf.Session() as sess:
+  feats, labs =sess.run(my_input_fn())
+  print(len(feats), len(feats['mfccs'][0]))
+
+# Create the feature_columns, which specifies the input to our model.
+# All our input features are numeric, so use numeric_column for each one.
+# I have 377 floats for each mfcc window
+feature_columns = [tf.feature_column.numeric_column(key='mfccs', dtype=tf.float64)]
 
 
-exit()
+# Use the DNNClassifier pre-made estimator
+classifier = tf.estimator.DNNClassifier(
+  feature_columns=feature_columns, # The input features to our model
+  hidden_units=[10, 10], # Two layers, each with 10 neurons
+  n_classes=96,
+  model_dir='/tmp/') # Path to where checkpoints etc are stored
 
-# https://medium.com/@TalPerry/getting-text-into-tensorflow-with-the-dataset-api-ffb832c8bec6
+
+
+# Train our model, use the previously function my_input_fn
+# Input to training is a file with training example
+# Stop training after 8 iterations of train data (epochs)
+
+with tf.Session() as sess:
+  sess.run(
+    classifier.train(
+      input_fn=lambda: my_input_fn()
+    )
+  )
+
+exit()                                                                                         
+
+# def train_kmeans(batch):
+#     kmeansEstimator = tf.contrib.factorization.KMeansClustering(num_clusters=1000, use_mini_batch=False)
+#     kmeansEstimator.train(batch)
+#     centers = kmeansEstimator.cluster_centers()
+#     return centers
