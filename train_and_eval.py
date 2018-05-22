aeb4b8329a92fe2fb088d3d5a7003cb4e25bffe7
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-05-22 17:40:41 +0000

    each step is a batch, not an epoch! I had one batch of one example, and so only one cluster was getting trained, not its working :))))

--- train_and_eval.py
+++ train_and_eval.py
@@ -50,56 +50,57 @@ def zscore(in_tensor):
       tf.reduce_min(in_tensor)
     )
   )
-  return(in_tensor)
+  return(out_tensor)
 
   
 
-# # K-Means
+# K-Means
 
-# train_spec_kmeans = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/csv.tfrecords', 'kmeans') , max_steps=1)
-# eval_spec_kmeans = tf.estimator.EvalSpec(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'kmeans') )
+train_spec_kmeans = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/csv.tfrecords', 'kmeans') , max_steps=5000)
+eval_spec_kmeans = tf.estimator.EvalSpec(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'kmeans') )
 
-# KMeansEstimator = tf.contrib.factorization.KMeansClustering(
-#   num_clusters=500,
-#   feature_columns = [tf.feature_column.numeric_column(
-#     key='mfccs',
-#     dtype=tf.float64,
-#     shape=(377,),
-#     normalizer_fn =  lambda x: zscore(x)
-#   )], # The input features to our model
-#   use_mini_batch=False)
+KMeansEstimator = tf.contrib.factorization.KMeansClustering(
+  num_clusters=96,
+  feature_columns = [tf.feature_column.numeric_column(
+    key='mfccs',
+    dtype=tf.float64,
+    shape=(377,),
+    normalizer_fn =  lambda x: x # zscore(x)
+  )], # The input features to our model
+  use_mini_batch=True)
 
-# print("Train and Evaluate K-Means")
-# tf.estimator.train_and_evaluate(KMeansEstimator, train_spec_kmeans, eval_spec_kmeans)
+print("Train and Evaluate K-Means")
+tf.estimator.train_and_evaluate(KMeansEstimator, train_spec_kmeans, eval_spec_kmeans)
 
-# # map the input points to their clusters
-# cluster_centers = KMeansEstimator.cluster_centers()
-# cluster_indices = list(KMeansEstimator.predict_cluster_index(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'kmeans')))
-# for i in cluster_indices:
-#   print(i)
-
-
+# map the input points to their clusters
+cluster_centers = KMeansEstimator.cluster_centers()
+cluster_indices = list(KMeansEstimator.predict_cluster_index(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'kmeans')))
+for i in cluster_indices:
+  print(i)
 
 
-# DNN
-
-# Define train and eval specs
-train_spec_dnn = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/train.tfrecords', 'dnn') , max_steps=100000)
-eval_spec_dnn = tf.estimator.EvalSpec(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'dnn') )
-
-DNNClassifier = tf.estimator.DNNClassifier(
-  feature_columns = [tf.feature_column.numeric_column(key='mfccs', dtype=tf.float64, shape=(377,))], # The input features to our model
-  hidden_units = [256, 256, 256, 256], # Two layers, each with 10 neurons
-  n_classes = 96,
-  model_dir = '/tmp/tf') # Path to where checkpoints etc are stored
 
 
-print("Train and Evaluate DNN")
-tf.estimator.train_and_evaluate(DNNClassifier, train_spec_dnn, eval_spec_dnn)
+# # DNN
 
-predictions = list(DNNClassifier.predict(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'dnn')))
+# # Define train and eval specs
+# train_spec_dnn = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/train.tfrecords', 'dnn') , max_steps=100000)
+# eval_spec_dnn = tf.estimator.EvalSpec(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'dnn') )
 
-for logits in predictions:
-  print(logits['probabilities'])
+# DNNClassifier = tf.estimator.DNNClassifier(
+#   feature_columns = [tf.feature_column.numeric_column(key='mfccs', dtype=tf.float64, shape=(377,))], # The input features to our model
+#   hidden_units = [256, 256, 256, 256], # Two layers, each with 10 neurons
+#   n_classes = 96,
+#   model_dir = '/tmp/tf') # Path to where checkpoints etc are stored
+
+
+
+# print("Train and Evaluate DNN")
+# tf.estimator.train_and_evaluate(DNNClassifier, train_spec_dnn, eval_spec_dnn)
+
+# predictions = list(DNNClassifier.predict(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'dnn')))
+
+# for logits in predictions:
+#   print(logits['probabilities'])
   
 
