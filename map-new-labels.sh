3973fdf6c13d20ba8c17f42c42efb5b4a8c631fc
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-06-19 12:56:31 +0000

    comments

--- map-new-labels.sh
+++ map-new-labels.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Joshua Meyer 2018
+
+# given new mappings (a 2-col txt file)
+#       old kaldi ark file (in txt format)
+#
+# search replace via sed
+
 
 MAPPINGS=$1
 TXTARK=$2
@@ -7,8 +14,6 @@ while read mapping; do
     mapArr=($mapping)
     old=${mapArr[0]}
     new=${mapArr[1]}
-    
-
 
     sed -Ei "s/dim=736 \[ ${old} 1 \]/dim=736 \[ ${new} 1 \]/g" $TXTARK
 
