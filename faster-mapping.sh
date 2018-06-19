8a4e06648e623703ed347c51d45714401f10f7a9
Ubuntu <ubuntu@ip-172-31-29-116.us-west-2.compute.internal>
2018-06-19 17:39:25 +0000

    much faster

---
+++ faster-mapping.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "### SPLIT ARK FOR MULTIPLE JOBS ###"
+
+
+ARKFILE=$1
+MAPPINGS=$2
+
+
+num_lines=(`wc -l $ARKFILE`)
+num_processors=(`nproc`)
+segs_per_job=$(( num_lines / num_processors ))
+
+echo "$0: processing $num_lines segments from $ARKFILE"
+echo "$0: splitting segments over $num_processors CPUs"
+echo "$0: with $segs_per_job segments per job."
+# will split into segments00 segments01 ... etc
+split -l $segs_per_job --numeric-suffixes --additional-suffix=.tmp $ARKFILE ARK_split
+
+
+
+
+
+# make an array for proc ids
+proc_ids=()
+
+for i in ARK_split*.tmp; do
+    while read mapping; do
+	mapArr=($mapping)
+	old=${mapArr[0]}
+	new=${mapArr[1]}
+	sed -Ei "s/dim=736 \[ ${old} 1 \]/dim=736 \[ ${new} 1 \]/g" $i
+    done <$MAPPINGS &
+    proc_ids+=($!)
+done
+
+# wait for subprocesses to stop
+for proc_id in ${proc_ids[*]}; do wait $proc_id; done;
+
+
+
