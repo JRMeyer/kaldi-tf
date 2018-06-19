8930825de68788172e7f8a06f1adb14d0d731700
Ubuntu <ubuntu@ip-172-31-18-244.us-west-2.compute.internal>
2018-06-19 12:58:45 +0000

    just transfering data between instances via github

--- train_and_eval.py
+++ train_and_eval.py
@@ -72,7 +72,7 @@ def zscore(in_tensor):
 
 # K-Means
 
-train_spec_kmeans = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/train.tfrecords', 'kmeans') , max_steps=1000)
+train_spec_kmeans = tf.estimator.TrainSpec(input_fn = lambda: my_input_fn('/home/ubuntu/train.tfrecords', 'kmeans') , max_steps=100000)
 eval_spec_kmeans = tf.estimator.EvalSpec(input_fn = lambda: my_input_fn('/home/ubuntu/eval.tfrecords', 'kmeans') )
 
 KMeansEstimator = tf.contrib.factorization.KMeansClustering(
